c7679c46a50dec8648c700723d34d67fb614de6e
Vojtech Masek <vojtech.masek@flowup.cz>
2018-03-11 22:02:52 +0100

    test(): async execution of tests with improved log
    Signed-off-by: Vojtech Masek <vojtech.masek@flowup.cz>

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,11 +1,10 @@
-import { compare, DiffSet, Result, State } from 'dir-compare';
+import { compare, DiffSet, State } from 'dir-compare';
 import { exists, mkdir } from 'fs';
 import * as rimraf from 'rimraf';
 import { promisify } from 'util';
 import { generateAPIClient } from '../src/generator';
 
-// const testReferences = ['esquare', 'gcloud-firestore', 'sports'];
-const testReferences = ['esquare'];
+const testReferences = ['esquare', 'gcloud-firestore'];
 
 const compareOptions = {compareSize: true};
 const stateSymbols: {[key in State]: string} = {
@@ -19,46 +18,46 @@ async function runTests(): Promise<void> {
   const testsOutDir = `${__dirname}/tests-output`;
 
   if (await promisify(exists)(testsOutDir)) {
-    await promisify(rimraf)(testsOutDir)
-      .then(() => {
-        console.info('test output deleted');
-      });
+    await promisify(rimraf)(testsOutDir);
   }
 
   await promisify(mkdir)(testsOutDir);
 
-  await testReferences.forEach(async (reference) => {
+  await Promise.all(testReferences.map(async (reference) => {
     console.log(`Running test for ${reference}`);
 
     const refDir = `${__dirname}/${reference}`;
-    const genDir = `${testsOutDir}/${reference}/api`;
+    const genDir = `${testsOutDir}/${reference}`;
 
-    await generateAPIClient(`${refDir}/swagger.yaml`, genDir);
+    await generateAPIClient(`${refDir}/swagger.yaml`, genDir)
+      .catch((err: Error) => console.error(`Error has occurred while generating api client for ${reference}`, err));
 
-    compare(`${refDir}/api`, genDir, compareOptions).then(
-      ({same, equal, distinct, differences, left, right, diffSet}: Result) => {
-        if (!same) {
-          console.group(`Stats for ${reference}`);
-          console.log(`equal: ${equal}`);
-          console.log(`distinct: ${distinct}`);
-          console.log(`left: ${left}`);
-          console.log(`right: ${right}`);
-          console.log(`differences: ${differences}\n`);
+    const {same, equal, distinct, differences, left, right, diffSet} = await compare(`${refDir}/api`, genDir, compareOptions);
 
-          console.log('[ reference dir ]         [ test dir ]');
-          diffSet.forEach(({name1, name2, state, type1, type2}: DiffSet) => {
-            if (stateSymbols[state] !== stateSymbols.equal) {
-              console.log(`(${type1}) ${name1}  ${stateSymbols[state]}  ${name2} (${type2})`);
-            }
-          });
+    if (!same) {
+      console.group(`Stats for ${reference}`);
+      console.log(`equal: ${equal}`);
+      console.log(`distinct: ${distinct}`);
+      console.log(`left: ${left}`);
+      console.log(`right: ${right}`);
+      console.log(`differences: ${differences}\n`);
 
-          console.groupEnd();
-        } else {
-          console.log(`Test for ${reference} has successfully passed`);
+      console.log('[ reference dir ]         [ test dir ]');
+      diffSet.forEach(({name1, name2, state, type1, type2}: DiffSet) => {
+        if (stateSymbols[state] !== stateSymbols.equal) {
+          console.log(`(${type1}) ${name1}  ${stateSymbols[state]}  ${name2} (${type2})`);
         }
-      }
-    );
-  });
+      });
+
+      console.log(`\nUse diff on folders below to acquire more info`);
+      console.log(`diff ${refDir}/api ${genDir}\n\n`);
+
+      console.groupEnd();
+    } else {
+      console.log(`Test for ${reference} has successfully passed\n\n`);
+      await promisify(rimraf)(genDir);
+    }
+  }));
 }
 
-runTests().then(() => console.info('Tests execution has ended'));
+runTests().then(() => console.info('Tests execution has ended successfully'));
