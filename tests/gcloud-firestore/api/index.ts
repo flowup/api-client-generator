42cf4c040615b2675ef9957e6f2368c76596cef6
Stefan Warman <stefan.warman@gmail.com>
2018-03-12 18:41:24 +0100

    fix(api-client): models are not imported if there are none
    * Default to relative path instead of localhost for domain.
    
    * Remove window declare (not needed). Use shorter syntax for setting default address
    
    * Implement default hostname in generator instead of template
    
    * Fixes to domain generation
    
    * Added commnets to generator
    
    * Undo unintended change to template
    
    * Fixed import path to APIClient
    
    * Fixed #31 - array parameters not being added correctly. Rebuilt generated test code.
    
    * Update parser.ts
    
    * test(gcloud-firestore): any corrected to string

--- tests/gcloud-firestore/api/index.ts
+++ tests/gcloud-firestore/api/index.ts
@@ -5,6 +5,7 @@ import { HttpHeaders, HttpParams } from '@angular/common/http';
 import { APIClient, USE_DOMAIN, USE_HTTP_OPTIONS } from './api-client.service';
 
 export * from './models';
+
 export { APIClient } from './api-client.service';
 
 export interface HttpOptions {
