5bcadfa26f2b961482dd1de1f3b65fcee5ed8bda
Vojtech Masek <vojtech.masek@flowup.cz>
2018-06-08 10:18:20 +0200

    test(custom): added customer extending model interface
    Signed-off-by: Vojtech Masek <vojtech.masek@flowup.cz>

---
+++ tests/custom/api/models/customer.model.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+import {
+  model,
+} from './..';
+
+export interface Customer extends model {
+  address: string;
+  name: string;
+}
---
+++ tests/custom/api/models/model.model.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+
+export interface Model {
+  created: number;
+  deleted: number;
+  id: string;
+  updated: number;
+}
