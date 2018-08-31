369b560bbf357696da5098038ca4193d29c96b1f
Vojtech Mašek <vojtech.masek@flowup.cz>
2018-08-31 18:05:20 +0200

    tests(): model exports sorted
    Signed-off-by: Vojtech Mašek <vojtech.masek@flowup.cz>

--- tests/custom/api/models/index.ts
+++ tests/custom/api/models/index.ts
@@ -1,16 +1,16 @@
 /* tslint:disable */
 
-export { ItemList } from './item-list.model';
-export { ItemModelList } from './item-model-list.model';
-export { MyInterface } from './my-interface.model';
+export { Cat } from './cat.model';
+export { Customer } from './customer.model';
 export { Data } from './data.model';
 export { DataModel } from './data-model.model';
-export { TestModel } from './test-model.model';
-export { Pet } from './pet.model';
 export { Dog } from './dog.model';
-export { Cat } from './cat.model';
-export { Mouse } from './mouse.model';
+export { ItemList } from './item-list.model';
+export { ItemModelList } from './item-model-list.model';
 export { Model } from './model.model';
-export { Customer } from './customer.model';
-export { Right } from './right.enum';
+export { Mouse } from './mouse.model';
+export { MyInterface } from './my-interface.model';
+export { Pet } from './pet.model';
 export { Possition } from './possition.enum';
+export { Right } from './right.enum';
+export { TestModel } from './test-model.model';
--- tests/filtered-api/api/models/index.ts
+++ tests/filtered-api/api/models/index.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 
+export { DummySelectorSettings } from './dummy-selector-settings.model';
 export { DummySelectorViewModel } from './dummy-selector-view-model.model';
 export { DummyViewModel } from './dummy-view-model.model';
-export { DummySelectorSettings } from './dummy-selector-settings.model';
 export { ProjectTypeViewModel } from './project-type-view-model.model';
