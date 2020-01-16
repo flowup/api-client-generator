6c87e1a4458ecc543f35b6f41cccf256d9ed71f7
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-01-16 15:59:20 +0100

    test(integration): guarded API client build

--- tests/test-angular-project/src/app/app.module.ts
+++ tests/test-angular-project/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APIClientModule } from '../api-no-tags';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, APIClientModule.forRoot({ guardResponses: true })],
   providers: [],
   bootstrap: [AppComponent],
 })
