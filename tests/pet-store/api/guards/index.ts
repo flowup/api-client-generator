5ec19104d4a23e26f61697553a5c0ded112ebb2a
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-12-02 14:12:55 +0100

    refactor(guards): built in guard for file and blob removed
    closes #102

--- tests/pet-store/api/guards/index.ts
+++ tests/pet-store/api/guards/index.ts
@@ -2,16 +2,6 @@
 
 import * as models from '../models';
 
-/* pre-prepared guards for build in complex types */
-
-function _isBlob(arg: any): arg is Blob {
-  return arg != null && typeof arg.size === 'number' && typeof arg.type === 'string' && typeof arg.slice === 'function';
-}
-
-export function isFile(arg: any): arg is File {
-return arg != null && typeof arg.lastModified === 'number' && typeof arg.name === 'string' && _isBlob(arg);
-}
-
 /* generated type guards */
 
 export function isApiResponse(arg: any): arg is models.ApiResponse {
