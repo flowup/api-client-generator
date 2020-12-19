dadedcb27c596ca20d5102ad671e61d65377b03c
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-12-19 18:35:11 +0100

    test(angular-project): updated to angular 11
    closes #107

--- tests/test-angular-project/src/app/app.component.spec.ts
+++ tests/test-angular-project/src/app/app.component.spec.ts
@@ -1,38 +1,52 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [AppModule],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [AppModule],
+      }).compileComponents();
+    }),
+  );
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it(
+    'should create the app',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    }),
+  );
 
-  it(`should have as title 'test-angular-project'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-angular-project');
-  }));
+  it(
+    `should have as title 'test-angular-project'`,
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('test-angular-project');
+    }),
+  );
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to test-angular-project!',
-    );
-  }));
+  it(
+    'should render title in a h1 tag',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain(
+        'Welcome to test-angular-project!',
+      );
+    }),
+  );
 
-  it(`should have list of pet store domains`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.domains).toEqual(['pet.domain', 'user.domain']);
-  }));
+  it(
+    `should have list of pet store domains`,
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.domains).toEqual(['pet.domain', 'user.domain']);
+    }),
+  );
 });
