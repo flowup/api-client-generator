1873f0011df67a875ae4ce1413020b08a65bf9e5
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-12-19 17:40:42 +0100

    test(): suppressed console log, info and warn in tests

--- setupJest.ts
+++ setupJest.ts
@@ -1,2 +1,6 @@
 // tslint:disable:no-import-side-effect
 import 'jest-preset-typescript';
+
+jest.spyOn(global.console, 'log').mockImplementation(() => jest.fn());
+jest.spyOn(global.console, 'info').mockImplementation(() => jest.fn());
+jest.spyOn(global.console, 'warn').mockImplementation(() => jest.fn());
