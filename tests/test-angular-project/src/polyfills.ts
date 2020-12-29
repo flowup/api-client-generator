1b0b3b58cdfc34081553995f313a29c222419cb0
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-12-29 15:38:41 +0100

    refactor(tests): unnecessary polyfill removed

--- tests/test-angular-project/src/polyfills.ts
+++ tests/test-angular-project/src/polyfills.ts
@@ -18,10 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
-
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
